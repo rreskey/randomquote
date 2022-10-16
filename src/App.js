57f9dff22e53474bab7808921f6d2543df98d623
rreskey <smivasia@gmail.com>
2022-10-16 15:32:31 +0300

    version 1.0

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Quote from "./WikiQuote/Quote";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
 
 function App() {
   return (
   <div className="wrapper">
       <Quote />
   </div>
-    
   );
 }
 
--- src/WikiQuote/Quote.js
+++ src/WikiQuote/Quote.js
@@ -1,5 +1,5 @@
 import React , {useEffect, useState} from "react";
-import { ReactPropTypes } from "react";
+import Button from 'react-bootstrap/Button'
 
 function Quote() {
     const [data, setData] = React.useState(null)
@@ -9,6 +9,7 @@ function Quote() {
             const response = await fetch('https://api.quotable.io/random');
             const data = await response.json();
             setData(data)
+
         } catch (error) {
             console.error();
             setData({content: 'Something went wrong...'})
@@ -19,21 +20,45 @@ function Quote() {
         updateQuote()
     }, [])
 
-    if (!data) return null;
-
     const styles = {
         ul: {
-            listStyle: 'none',
-        }
+            listStyle: 'none', 
+            padding: '3rem'
+        },
+
+        quoteData: {
+            fontSize: '2.2rem',
+            textAlign: 'left',
+        },
+
+        newQuote: {
+            fontSize: '2.2rem',
+            textAlign: 'center',
+            marginBottom: '10px',
+            width: '90%',
+            display: 'block',
+            margin: '0 auto'
+        },
+
+        link: {
+            textDecoration: 'none',
+            color: 'gray'
+        },
     }
 
+    if (!data) return null;
+
+    let url = 'https://en.wikipedia.org/wiki/';
+    
     return (
         <div className="quote">
-            <ul style={styles}>
-                <p>{data.content}</p>
-                <p className="author">{data.author}</p>
-                <button type='button' onClick={updateQuote}>Get a random Quote</button>
+            <ul style={styles.ul}>
+                <li>
+                    <p style={styles.quoteData}>{data.content}</p>
+                    <p className="author"><a href={url+data.author} target="_blank" style={styles.link}>{data.author}</a></p>
+                </li>
             </ul>
+            <Button type='button' variant='success' size='lg' style={styles.newQuote} onClick={updateQuote}>Get a random Quote</Button>
         </div>
     )
 }
